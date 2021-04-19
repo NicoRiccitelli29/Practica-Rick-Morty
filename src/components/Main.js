f0d3109c37d2bf90cf8ef7c6826793ae4b56532c
NicoRiccitelli29 <nriccitelli@udesa.edu.ar>
2021-04-19 17:08:50 -0300

    Cambio de componententes!

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,68 @@
 import Tarjeta from './Tarjeta'
-import rickandmorty from '../rickandmorty.json'
+//import rickandmorty from '../rickandmorty.json'
+import React, {Component} from 'react'; 
 
 
-function Main(props){
+class Main extends Component{
+    constructor() {
+        super();
+        this.state = {
+            items: [],
+           contador: 0
+        }
+    }
+    componentDidMount(){
+        this.resetDefault();
+    }
+    
+
+    resetDefault() {
+        fetch('https://rickandmortyapi.com/api/character/?page=1')
+        .then(result => result.json())
+        .then(data => {
+            this.setState({items : data.results, nextPage: data.info.next, lastPage: data.info.last, contador: 0});
+            console.log(data.results);
+        })
+        
+    }
+    agregarTarjeta(){
+        fetch(this.state.nextPage)
+            .then(result => result.json())
+            .then(data => {
+                this.setState({items : data.results, nextPage: data.info.next, lastPage: data.info.last});
+                console.log(data.results);
+            })
+        }
+
+    Borrar(idTarjeta){
+               let resultado = this.state.items.filter((item)=>{
+                    return item.id !== idTarjeta
+                })
+                this.setState({contador: this.state.contador +1 ,items: resultado})
+                console.log("Tarjeta a borrar: "+ idTarjeta);
+                console.log(this.state.contador);
+                
+        }
+   
+    render(){
     return(
         <div>
+            <div>
+            <button className="botonReset" onClick={this.resetDefault.bind(this)}>Reset</button>
+            <button className="botonMore" onClick={this.agregarTarjeta.bind(this)}>More Cards</button>
+            <div className= "contador">
+                Tajetas eliminadas : {this.state.contador}
+            </div>
+            </div>
            { 
-            rickandmorty.map((unaTarjeta, idx) =>{
-                return(<Tarjeta  key={idx} infoTarjeta={unaTarjeta}/>)
+            this.state.items.map((unaTarjeta, idx) =>{
+                return(<Tarjeta  key={idx} infoTarjeta={unaTarjeta} borrar={this.Borrar.bind(this)} colorFondo="white"/>)
             })
            }
+          
         </div>
    )
+ } 
 }
+
 export default Main
--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -1,13 +1,46 @@
-function Tarjeta(props){
-    return(
-        <div className="card-body">
-                <img src={props.infoTarjeta.image} alt=""/>
-                <li className="list-group-item">Nombre:{props.infoTarjeta.name}</li>
-                <li className="list-group-item">Alive:{props.infoTarjeta.species}</li>
-                <li className="list-group-item">Origin:{props.infoTarjeta.origin.name}</li>
-                
+import React, {Component} from 'react'; 
+
+class Tarjeta extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            colorInicial:this.props.colorFondo,
+            colorFondo : this.props.colorFondo,
+        }
+    }
+    cambiarColor(color){
+        if(this.state.colorFondo !=="orange")
+        this.setState({
+            colorFondo: color,
+        })
+        else
+        this.setState({
+            colorFondo: "orange",
+        })
+        console.log("Cambiamos el color por " + color);
+    }
+   borrarItem(){
+       this.props.borrar(this.props.infoTarjeta.id)
+
+   }
+  
+ 
+    
+
+    render(){
+        return(
+            <div className="card-body" style={{backgroundColor: this.state.colorFondo}}
+            onMouseEnter={() => this.cambiarColor("pink")}
+            onMouseLeave={() => this.cambiarColor("white")}>
+                    <img src={this.props.infoTarjeta.image} alt=""/>
+                    <li className="list-group-item">Nombre:{this.props.infoTarjeta.name}</li>
+                    <li className="list-group-item">Alive:{this.props.infoTarjeta.species}</li>
+                    <li className="list-group-item">Origin:{this.props.infoTarjeta.origin.name}</li>
+                    <button className="boton" onClick={() => this.cambiarColor("orange")}>Cambiar color</button>
+                    <button className="boton" onClick={this.borrarItem.bind(this, this.props.id)}>Borrar Tarjeta</button>
             </div>
-    )
+         )
+    }
 }
 
-export default Tarjeta
+export default Tarjeta;
